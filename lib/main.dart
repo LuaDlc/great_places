6ebf57c0212c453a1db6e9707ab572d8b2594d41
Luana Dias <luana.dias@colab.re>
2024-03-29 10:40:16 -0300

    fix: adicionando o change notifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:great_places/pages/place_form_page.dart';
 import 'package:great_places/pages/places_list_page.dart';
+import 'package:great_places/providers/greate_places.dart';
 import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,16 +15,19 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Great Places',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatePlaces(),
+      child: MaterialApp(
+        title: 'Great Places',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const PlacesListPage(),
+        routes: {
+          AppRoutes.placeForm: (ctx) => const PlaceFormPage(),
+        },
       ),
-      home: const PlacesListPage(),
-      routes: {
-        AppRoutes.placeForm: (ctx) => const PlaceFormPage(),
-      },
     );
   }
 }
