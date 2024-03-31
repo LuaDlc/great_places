9e6c5e073d736a8abd4ca533bdcf8408891d29f1
Luana Dias <luana.dias@colab.re>
2024-03-31 18:04:24 -0300

    fix

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -3,9 +3,25 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:great_places/models/places.dart';
+import 'package:great_places/utils/db_util.dart';
 
 class GreatePlaces with ChangeNotifier {
   List<Place> _items = [];
+//funcao responsavel por carregar os lugares
+  Future<void> loadPlaces() async {
+    final dataList = await DbUtil.getData('places');
+    _items = dataList
+        .map((item) => Place(
+              id: item['id'],
+              title: item['title'],
+              location: item['location'],
+              image: File(
+                item['image'],
+              ),
+            ))
+        .toList();
+    notifyListeners();
+  }
 
   List<Place> get items {
     return [..._items];
@@ -30,6 +46,11 @@ class GreatePlaces with ChangeNotifier {
         location: null,
         image: image);
     _items.add(newPlace);
+    DbUtil.insert('places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+    });
     notifyListeners(); //para notificar os listeners que a lista foi alterada e atualizar os componentes
   }
 
---
+++ lib/widgets/location_input.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/utils/location_util.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({super.key});
+
+  @override
+  State<LocationInput> createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String?
+      _previewImageUrl; //api do google fornecera uma preview da iamgem setada
+
+  Future<void> _getCurrentUserLocation() async {
+    final locData = await Location().getLocation();
+
+    final staticMapImageUrl = LocationUtil.generateLocationPreviewImage(
+        latitude: locData.latitude, longitude: locData.longitude);
+
+    setState(() {
+      _previewImageUrl = staticMapImageUrl;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //preview da imagem
+    return Column(
+      children: [
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration:
+              BoxDecoration(border: Border.all(width: 1, color: Colors.grey)),
+          child: _previewImageUrl == null
+              ? const Text('Localizacao nao informada')
+              : Image.network(
+                  _previewImageUrl!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: _getCurrentUserLocation, //referencia da funcao
+              icon: const Icon(Icons.location_on),
+              label: const Text(
+                'Localizacao atual',
+                style: TextStyle(),
+              ),
+            ),
+            TextButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.map),
+              label: const Text('Selecione no mapa'),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
